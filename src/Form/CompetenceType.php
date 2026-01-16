1c12af968fd6fb0867f05c7b295a2bdaa4f0e548
Veronika <bwb.mikova.v@fondespierre.com>
2026-01-16 16:36:12 +0100

    création CRUD Competence

---
+++ src/Form/CompetenceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Candidat;
+use App\Entity\Competence;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompetenceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('categorie')
+            ->add('nom')
+            ->add('candidats', EntityType::class, [
+                'class' => Candidat::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Competence::class,
+        ]);
+    }
+}
