972476e0c2ce675aaff20face0764acea1a62900
Veronika <bwb.mikova.v@fondespierre.com>
2026-01-23 15:43:57 +0100

    departement : correction route

--- src/Controller/DepartementController.php
+++ src/Controller/DepartementController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/departement')]
+#[Route('/api/departement')]
 final class DepartementController extends AbstractController
 {
 
