b55a9f82391be6b8ee43ab95746a2b5248ec5933
Veronika <bwb.mikova.v@fondespierre.com>
2026-01-23 11:33:53 +0100

    modif route

--- src/Controller/CompetenceController.php
+++ src/Controller/CompetenceController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/competence')]
+#[Route('/api/competence')]
 final class CompetenceController extends AbstractController
 {
 
