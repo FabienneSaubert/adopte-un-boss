96e863529f751452808aac86e67d5c057b00ecd6
Delphine Wolf <bwb.wolf.d@fondespierre.com>
2026-02-02 11:01:24 +0100

    changement dans le dossier controller competence de la route

--- src/Controller/CompetenceController.php
+++ src/Controller/CompetenceController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/domaine')]
+#[Route('/api/competence')]
 final class CompetenceController extends AbstractController
 {
 
