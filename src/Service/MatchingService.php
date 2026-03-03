e0a56e41f662eb0536bdac0a7f0b5afab9eadc14
Florian Garcia Soto <floriangarciasoto@outlook.fr>
2026-03-03 10:23:53 +0100

    Ajout de l'algorithme de matching avec prise en compte des coefficients pour le département, le niveau d'études et les compétences

--- src/Service/MatchingService.php
+++ src/Service/MatchingService.php
@@ -10,10 +10,8 @@ class MatchingService
     /**
      * Calcule un score de matching entre un candidat et une liste d'offres
      *
-     * @param Candidat $candidat
-     * @param Offre[] $offres
-     *
-     * @return array Tableau d'offres triées
+     * @param Candidat $candidat instance du candidat connecté
+     * @param Offre[] $offres adresse mémoire du tableau des offres, celui-ci sera directement modifié par la méthode
      */
     public function match(Candidat $candidat, array &$offres): void
     {
@@ -35,8 +33,47 @@ class MatchingService
         // Initialisation du score
         $score = 0;
         
-        // ** Algorithme de matching ici **
-        $score = $offre->getId(); // temporaire, permet d'inverser la liste des offres pour un résultat visuel
+        // * Calcul du score - département *
+        // Si le département a été défini des deux côtés
+        if ($candidat->getDepartement() !== null && $offre->getDepartement() !== null) {
+            // Si l'offre est dans le même département que le candidat
+            if ($candidat->getDepartement()->getId() === $offre->getDepartement()->getId()) {
+                // Si le coefficient du département est correctement défini
+                if ($offre->getCoeffDepartement() !== null) {
+                    // On le prend pour le calcul du score
+                    $score += $offre->getCoeffDepartement();
+                }
+            }
+        }
+
+        // * Calcul du score - niveau d'études *
+        // Si le niveau d'études a été défini chez le candidat
+        if ($candidat->getNiveauEtude() !== null) {
+            // Si l'offre correspond au même niveau d'étude que le candidat
+            if ($candidat->getNiveauEtude() === $offre->getNiveauEtudes()) {
+                // Si le coefficient du niveau d'études est correctement défini
+                if ($offre->getCoeffNiveauEtudes() !== null) {
+                    // On le prend pour le calcul du score
+                    $score += $offre->getCoeffNiveauEtudes();
+                }
+            }
+        }
+
+        // * Calcul du score - compétences *
+        // Pour chaque compétence du candidat
+        foreach ($candidat->getCompetences() as $competence) {
+            // Pour chaque selection de compétence de l'offre
+            foreach ($offre->getSelectionCompetences() as $selectionCompetence) {
+                // Si l'ID de la compétence du candidat correspond à l'ID de la compétence seléctionnée
+                if ($competence->getId() === $selectionCompetence->getCompetence()?->getId()) {
+                    // Si le coefficient de la seléction de la compétence est correctement défini
+                    if ($selectionCompetence->getCoeffCompetence() !== null) {
+                        // On le prend pour le calcul du score
+                        $score += $selectionCompetence->getCoeffCompetence();
+                    }
+                }
+            }
+        }
 
         // Renvoi du score calculé selon l'algorithme de matching
         return $score;
