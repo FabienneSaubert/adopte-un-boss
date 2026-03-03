c145a28cfcc7f0463eb8c8afadb3ae1c8efd4554
Florian Garcia Soto <floriangarciasoto@outlook.fr>
2026-03-03 11:06:26 +0100

    Mise en place d'un système de pagination pour la liste des offres

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -25,15 +25,23 @@ use Symfony\Component\Routing\Attribute\Route;
 final class OffreController extends AbstractController
 {
     #[Route(name: 'api_offre_get_collection', methods: ['GET'])]
-    public function index(OffreRepository $offreRepository, MatchingService $matchingService): JsonResponse
+    public function index(
+        Request $request,
+        OffreRepository $offreRepository,
+        MatchingService $matchingService
+    ): JsonResponse
     {
-        // Recupération des offres
-        $offres = $offreRepository->findAll();
+        // Récupération de la page par le query parameter p si défini, sinon 1 par défaut
+        $page = (int) $request->query->get('p', 1);
+        // Nombre maximal d'offres affichées sur une liste
+        $limite = 3;
+        // Recupération des offres paginnées
+        $offres = $offreRepository->findLatestPaginated($page,$limite);
         
         // Recupération de l'instance du candidat via l'access token envoyé par le client
         $candidat = $this->getCandidatConnecte();
 
-        // Si Lekiq a réussi à déterminer un candidat
+        // Si Lexiq a réussi à déterminer un candidat
         if ($candidat) {
             // C'est qu'il lui faut les offres triées avec le système de matching
             $matchingService->match($candidat,$offres);
